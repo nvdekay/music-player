22a27c4577aabe1b695eca48d2f6050033622905
nvdekay <khanhnvd.work@gmail.com>
2025-05-05 11:18:20 +0700

    updated songs name

--- main.js
+++ main.js
@@ -14,3 +14,48 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const app = {
+    songs: [
+        {
+            name: 'Du cho tan the',
+            singer: 'Erik',
+            path: './assets/music/Dù Cho Tận Thế.mp3',
+            image: './assets/img/Du cho tan the.jpg'
+        },
+        {
+            name: 'Dat nuoc tron niem vui',
+            singer: 'Ta Minh Tam',
+            path: './assets/music/Đất Nước Trọn Niềm Vui.mp3',
+            image: './assets/img/Dat nuoc tron niem vui.jpg'
+        },
+        {
+            name: 'Dung lam trai tim anh dau',
+            singer: 'Son Tung M-TP',
+            path: './assets/music/Đừng Làm Trái Tim Anh Đau.mp3',
+            image: './assets/img/Dung lam trai tim anh dau.jpg'
+        },
+        {
+            name: 'Ngay dau sau chia tay',
+            singer: 'Duc Phuc',
+            path: './assets/music/Ngay Đầu Sau Chia Tay.mp3',
+            image: './assets/img/Ngay dau sau chia tay.jpg'
+        },
+        {
+            name: 'Phep mau',
+            singer: 'Minh Toc',
+            path: './assets/music/Phép Màu.mp3',
+            image: './assets/img/Phep mau.jpg'
+        }
+    ],
+    render: function() {
+        
+    },
+
+    start: function() {
+        this.render();
+    }
+}
+
+app.start();
+
+
